c843ecfea89fbb9f586d798a2111f3f3d6dbbfe8
skirtapaieo <119537053+skirtapaieo@users.noreply.github.com>
2023-08-15 08:55:59 +0200

    bugfix

--- data/05MachineLearningAI/_machinelearningexpert/Math/probability-disease.py
+++ data/05MachineLearningAI/_machinelearningexpert/Math/probability-disease.py
@@ -1,4 +1,4 @@
-def probability_of_disease(accuracy, prevalence)
+def probability_of_disease(accuracy, prevalence):
 
     # Positive Predictive Value (PPV)
     ppv = (accuracy * prevalence) / (accuracy * prevalence + (1 - accuracy) * (1 - prevalence))
@@ -6,10 +6,9 @@ def probability_of_disease(accuracy, prevalence)
     # Negative Predictive Value (NPV)
     npv = (accuracy * (1 - prevalence)) / ((1 - accuracy) * prevalence + accuracy * (1 - prevalence))
 
-    return [round(ppv, 4), round(npv, 4)]
+    return [100*ppv, 100*npv]
 
 accuracy = 0.95
 prevalence = 0.03
 print(probability_of_disease(accuracy, prevalence))
-
 
