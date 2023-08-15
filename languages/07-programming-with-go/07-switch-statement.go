4ba4cfb1e8780cb94357949e8c4dada78bef0d76
skirtapaieo <119537053+skirtapaieo@users.noreply.github.com>
2023-08-15 08:11:28 +0200

    commented a line

--- languages/07-programming-with-go/07-switch-statement.go
+++ languages/07-programming-with-go/07-switch-statement.go
@@ -1,14 +1,15 @@
-package main
+/* package main
 
 import "fmt"
 
-func main() {
+func mains() {
 	a := 1
 	switch a {
 	case 1:
 		fmt.Println("a is 1")
 		fallthrough
-	case a < 2:
+	case a == 2:
 		fmt.Println("a is 2")
 	}
 }
+*/
