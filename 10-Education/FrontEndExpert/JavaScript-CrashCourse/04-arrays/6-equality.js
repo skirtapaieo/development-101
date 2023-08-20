c05adbaf98d51fa184401374b6a299f1f9285dc6
skirtapaieo <119537053+skirtapaieo@users.noreply.github.com>
2023-08-20 19:11:37 +0200

    changes

---
+++ 10-Education/FrontEndExpert/JavaScript-CrashCourse/04-arrays/6-equality.js
@@ -0,0 +1,23 @@
+
+/* console.log(5 == '5'); // true value only
+console.log(5 === '5'); // false type AND value
+
+console.log(NaN == NaN); // false
+console.log(NaN === NaN); // false
+
+console.log('abc' == NaN); // true
+
+console.log(null == undefined, null, undefined); // true
+console.log(null === undefined); // false */
+
+console.log({} == {})   // false
+
+
+// Which one should we use?
+
+// loose equality (==) is more common than strict equality (===)
+
+// strict equality (===) is more common than loose equality (==)
+
+// :-)
+
