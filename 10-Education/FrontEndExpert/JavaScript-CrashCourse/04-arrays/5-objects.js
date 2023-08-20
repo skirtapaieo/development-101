e6329df9c1570b29c4399b2e558360a7ac017e09
skirtapaieo <119537053+skirtapaieo@users.noreply.github.com>
2023-08-20 18:59:18 +0200

    added exercises

---
+++ 10-Education/FrontEndExpert/JavaScript-CrashCourse/04-arrays/5-objects.js
@@ -0,0 +1,50 @@
+
+/* const myKey = 'key';
+const website = {
+    name: 'algoexpert',
+    rating: 5,
+    founders: ['Neha', 'Sai'],
+    isAwesome: true,
+    [myKey]:1234
+  };
+
+  console.log(website); // object
+
+  console.log(website.name); // dot notation
+
+  website.name = 'algoexpert.åååå';
+  website.foo = 'bar';
+
+  console.log(website['name']); // bracket notation
+
+  console.log(website); */
+
+/*   const website = {
+    name: 'algoexpert',
+    rating: 5,
+    founders: ['Neha', 'Sai'],
+  };
+ */
+/* function Website(name, rating, founders)
+{
+    this.name = name;
+    this.rating = rating;
+    this.founders = founders;
+}
+ */
+
+/* const id = Symbol('id');
+const id2 = Symbol('id2');
+const id3 = Symbol('id3');
+const id4 = Symbol('id3');
+
+const obj = {
+    [id]: 1234,
+    [id2]: 1234,
+    id: 'hello',
+};
+
+console.log(obj);
+ */
+
+
