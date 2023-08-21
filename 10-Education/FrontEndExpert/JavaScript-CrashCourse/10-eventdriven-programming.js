bfea2b64c8b27b3a2695d07fadf6f33ca6c136c1
skirtapaieo <119537053+skirtapaieo@users.noreply.github.com>
2023-08-21 09:44:27 +0200

    minor changes

--- 10-Education/FrontEndExpert/JavaScript-CrashCourse/10-eventdriven-programming.js
+++ 10-Education/FrontEndExpert/JavaScript-CrashCourse/10-eventdriven-programming.js
@@ -1,14 +1,33 @@
-const button = document.querySelector('button');
+const button = document.querySelector('button'); // select the first button element
 
-button.addEventListener('click', onClick);
+// add event listener to window
+window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
+});
 
-document.body.addEventListener('click', onClick);
+// add event listener to the document
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM is fully loaded');
+});
+
+// add event listener to the body
+document.body.addEventListener('click', (event) => {
+    console.log('body was clicked');
+});
+
+button.addEventListener('click', onClick); // add event listener to the button
+
+document.body.addEventListener('click', onClick,{
+    capture: true, // capture the event in the capturing phase
+    once: true, // remove the event listener after the first time it is triggered
+    passive: true // the event listener will not call preventDefault()
+    signal: abortController.signal // abort the event listener when the signal is aborted
+}
+); // add event listener to the body
 
 function onClick(event) {
-    event.stopPropagation();
     console.log(event);
-    console.log(event.type);
     console.log(event.target);
-    console.log(this));
+    console.log(this);
     console.log('Button was clicked!');
 }
