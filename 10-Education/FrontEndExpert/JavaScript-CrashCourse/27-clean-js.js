8944bc5b478066c39cf891840188a7d7f40bd1d8
skirtapaieo <119537053+skirtapaieo@users.noreply.github.com>
2023-08-21 20:02:39 +0200

    clean code in javascript

---
+++ 10-Education/FrontEndExpert/JavaScript-CrashCourse/27-clean-js.js
@@ -0,0 +1,13 @@
+// easy to understand
+
+// modern syntax: Arrow functions, destructuring syntax
+
+// avoid nesting callbacks
+
+// avoid this ..
+
+// use functional programming - function chaining, pure functions for better code
+
+// code autoformatter - like prettier
+
+// style guides - use open source, like googles
