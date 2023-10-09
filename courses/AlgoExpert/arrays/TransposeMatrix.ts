7910e48e6b4f8d822b99b9a04dce26df5282dd3f
skirtapaieo <ptrksvnssnaieo@gmail.com>
2023-10-09 09:28:29 +0200

    transpose matrix exercise

--- courses/AlgoExpert/arrays/TransposeMatrix.ts
+++ courses/AlgoExpert/arrays/TransposeMatrix.ts
@@ -12,3 +12,24 @@ export function transposeMatrix(matrix: number[][]): number[][] {
   return transposed;
 }
 
+
+/**
+ * Name: Transpose Matrix (In-place)
+ * Problem: Given a square matrix, transpose it. That is, flip the matrix over its main diagonal.
+ */
+
+export function transposeMatrixInPlace(matrix: number[][]): void {
+  const n = matrix.length;
+
+  for (let i = 0; i < n; ++i) {
+    for (let j = i + 1; j < n; ++j) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]; // Swap elements
+    }
+  }
+}
+
+// Test case
+const mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+transposeMatrixInPlace(mat);
+console.log(mat);  // Output should be [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+
